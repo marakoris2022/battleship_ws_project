eb6113a7265f23ad96f70c6c53da6002ca9587f8
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-27 23:30:55 +0200

    feat: registration ready

---
+++ src/ws_server/index.js
@@ -0,0 +1,38 @@
+import { WebSocketServer } from "ws";
+
+const WS_PORT = 3000;
+
+const ws = new WebSocketServer({ port: WS_PORT });
+console.log(`WebSocketServer is up on the ${WS_PORT} port!`);
+
+ws.on("connection", (connection, request) => {
+  console.log("We have a connection");
+
+  connection.on("message", (data) => {
+    console.log("data", JSON.parse(data));
+    const dataFromClient = JSON.parse(data);
+    const requestType = dataFromClient.type;
+    const requestData = JSON.parse(dataFromClient.data);
+
+    connection.send(
+      JSON.stringify({
+        type: "reg",
+        data: JSON.stringify({
+          name: requestData.name,
+          index: 0,
+          error: false,
+          errorText: "",
+        }),
+        id: 0,
+      })
+    );
+  });
+
+  connection.on("close", () => {
+    console.log("closed");
+  });
+});
+
+ws.on("error", (error) => {
+  console.error("WebSocket error:", error);
+});
